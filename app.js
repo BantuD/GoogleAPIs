8561bd2c4ce14c68b87971229e797747bee4d7e3
BantuD <smarty.bantu@gmail.com>
2022-07-11 10:52:56 +0530

    Added Marker

--- app.js
+++ app.js
@@ -8,4 +8,21 @@ function initMap()
 
  }
  map = new google.maps.Map(document.getElementById("map"),options);
+
+ //Marker
+ const marker = new google.maps.Marker({
+   position:{lat:37.9922, lng:-1.1307},
+   map:map
+ });
+ //Marker End
+
+//Info window 
+const detailWindow = new google.maps.InfoWindow({
+   content: `<h2>Murcia City</h2>`
+});
+
+marker.addListener("mouseover",()=>{
+   detailWindow.open(map,marker);
+})
+
 }
